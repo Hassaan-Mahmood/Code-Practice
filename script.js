91d969d59fb37444c1c5590000a1778d7030051b
Hassaan Mahmood <hassaan2980@gmail.com>
2026-08-29 23:33:01 +0500

    Add console log for number 25

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ console.log(21)
 console.log(22)
 console.log(23)
 console.log(24)
+console.log(25)
 
 // =================================================================================================
 // Todo Tasks List
