80982f71f2fa38144518ec3caa2f00538e738a1d
Hassaan Mahmood <hassaan2980@gmail.com>
2026-09-06 22:42:27 +0500

    Add alert for 'hello world 7'

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ alert('hello world 3');
 alert('hello world 4');
 alert('hello world 5');
 alert('hello world 6');
+alert('hello world 7');
 
 
 // =================================================================================================
