9f572a062825a960a142bece97dfa399f4eb36a0
Hassaan Mahmood <hassaan2980@gmail.com>
2026-09-21 23:25:13 +0500

    Add console log for 'hello world 18'

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ console.log("hello world 16");
 
 console.log("hello world 17");
 
-
+console.log("hello world 18");
 // console.log("hello world 7");
 // Todo Tasks List
 // =================================================================================================
