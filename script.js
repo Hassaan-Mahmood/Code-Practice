5a86d06f4aa4d6f6e03a896bf3e167b02cfa20e0
Hassaan Mahmood <hassaan2980@gmail.com>
2026-09-23 23:39:14 +0500

    Add duplicate console.log statement

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ console.log("hello world");
 console.log("hello world");
 console.log("hello world");
 console.log("hello world");
-
+console.log("hello world");
 
 
 // console.log("hello world 7");
