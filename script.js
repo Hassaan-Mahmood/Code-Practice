efea005161b34602af31cd707e3817499f519556
Hassaan Mahmood <hassaan2980@gmail.com>
2026-09-24 23:35:11 +0500

    Refactor console log statements in script.js
    Removed an extra console log statement and added a new one.

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ console.log("hello world");
 console.log("hello world");
 
 console.log("hello world");
-
+console.log("hello world");
 // console.log("hello world 7");
 // Todo Tasks List
 // =================================================================================================
