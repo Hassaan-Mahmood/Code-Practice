b4c6ae2a6d780d8686767b6535b51573dd2cd45b
Hassaan Mahmood <hassaan2980@gmail.com>
2026-09-24 23:35:47 +0500

    Add duplicate console.log statement

--- script.js
+++ script.js
@@ -47,6 +47,7 @@ console.log("hello world");
 
 console.log("hello world");
 console.log("hello world");
+console.log("hello world");
 // console.log("hello world 7");
 // Todo Tasks List
 // =================================================================================================
