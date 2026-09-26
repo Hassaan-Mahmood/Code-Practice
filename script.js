a539c432c7316b1a3d5b50935e5f046258c3115a
Hassaan Mahmood <hassaan2980@gmail.com>
2026-09-26 23:51:20 +0500

    Remove duplicate console.log statements

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ console.log("hello world");
 console.log("hello world");
 console.log("hello world");
 ​console.log("hello world");
-
+console.log("hello world");
 // console.log("hello world 7");
 // Todo Tasks List
 // =================================================================================================
