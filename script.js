78ea79f3c8389050cb7a954eec41aed9303498cf
Hassaan Mahmood <hassaan2980@gmail.com>
2026-09-26 23:53:01 +0500

    Add additional console log statement

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ console.log("hello world");
 ​console.log("hello world");
 console.log("hello world");
 console.log("hello world");
+console.log("hello world");
+
 
 // console.log("hello world 7");
 // Todo Tasks List
